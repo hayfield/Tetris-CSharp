e5ab370832cc65803150913f77eb88773ca1d96d
hayfield <hayfield@gmx.com>
2011-05-05 08:18:21 +0100

    Removed some unused code and replaced a couple of magic numbers

--- Tetris/TetrisGame.cs
+++ Tetris/TetrisGame.cs
@@ -72,16 +72,6 @@ namespace Tetris
                     square.Left = col * squareDimensions;
                     string key = row.ToString() + col.ToString();
                     squares.Add(key, square);
-                    
-                    /*squares[row, col] = new Square(row, col);
-                    squares[row, col].Dock = DockStyle.Fill;
-                    squares[row, col].Margin = Padding.Empty;
-                    gameTable.SetCellPosition(squares[row, col], new TableLayoutPanelCellPosition(col, row));
-                    Square square = new Square(row, col);
-                    square.Dock = DockStyle.Fill;
-                    square.Margin = Padding.Empty;
-                    gameTable.Controls.Add(square, row, col);
-                    keys.Add("square" + row.ToString() + col.ToString());*/
                 }
             }
         }
@@ -110,48 +100,16 @@ namespace Tetris
         /// </summary>
         private void updateBoard()
         {
-            //gameTable.Controls.Clear();
-            //List<Square> sq = new List<Square>();
-            for (int row = 0; row < 20; row++)
+            // updates the color of each of the squares on the board
+            Square square;
+            for (int row = 0; row < board.board.GetLength(1) - board.hiddenRows; row++)
             {
-                for (int col = 0; col < 10; col++)
+                for (int col = 0; col < board.board.GetLength(0); col++)
                 {
-                    Square square;
                     squares.TryGetValue(row.ToString() + col.ToString(), out square);
                     square.color = board.board[col, row + board.hiddenRows];
-                    /*Square square = new Square(row, col);
-                    square.Dock = DockStyle.Fill;
-                    square.Margin = Padding.Empty;
-                    square.color = board.board[col, row + board.hiddenRows];
-                    gameTable.Controls.Add(square, row, col);
-                    sq.Add(square);*/
                 }
             }
-           /* int added = 0;
-            List<string> keys = new List<string>();
-            for (int row = 0; row < gameTable.RowCount; row++)
-            {
-                for (int col = 0; col < gameTable.ColumnCount; col++)
-                {
-                    // http://stackoverflow.com/questions/5634470/how-to-check-if-c-controlcollection-find-has-returned-a-result
-                    added++;
-                    //squares[row, col].BackColor = Color.FromArgb(board.board[row, col + board.hiddenRows]);
-                    var result = gameTable.Controls.Find("square" + row.ToString() + col.ToString(), true);
-                    if(result == null || result.Length == 0)
-                    {
-                        rowsClearedLabel.Text = "square" + row.ToString() + col.ToString();
-                        keys.Add(rowsClearedLabel.Text);
-                    }
-                    else
-                    {
-                        Square square = (Square)result[0];
-                        square.BackColor = Color.FromArgb(board.board[row, col + board.hiddenRows]);
-                    }
-                    
-                    //Square sq = new Square(2, 3);
-                    //sq.Parent = gameTable.SetCellPosition
-                }
-            }*/
         }
 
         private void newGameButton_Click(object sender, EventArgs e)
